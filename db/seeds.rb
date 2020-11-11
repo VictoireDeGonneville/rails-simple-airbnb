b54adba2a8fbb3fb21d72a5ed3a3ab2f464d0658
Victoire De Gonneville <v.dechampvallier@gmail.com>
2020-11-11 17:00:09 +0100

    airbnb simple

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Lake & Mountain Abisko',
+  address: '10 Abisko street',
+  description: 'an Amazing winter here in Abisko',
+  price_per_night: 100,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Romantic place Rome',
+  address: '20 Rome Street',
+  description: 'A lovely summer feel for this spacious garden flat.',
+  price_per_night: 50,
+  number_of_guests: 2
+)
+Flat.create!(
+  name: 'Amazing eiffel tower view Paris',
+  address: 'Champs de Mars 20',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, and an amzing view',
+  price_per_night: 200,
+  number_of_guests: 4
+)
